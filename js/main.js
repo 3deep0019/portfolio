7ad29160cfab6af3d87b885a4947fa6e5072e5f4
3deep0019 <41672219+3deep0019@users.noreply.github.com>
2020-01-16 21:27:28 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let nav_offset_top = $('.header-area').height() + 50;
+
+function navbarFixed() {
+    if ($('.header-area').length) {
+        $(window).scroll(function () {
+            let scroll = $(window).scrollTop();
+            if (scroll >= nav_offset_top) {
+                $('.header-area .main-menu').addClass('navbar-fixed');
+            } else {
+                $('.header-area .main-menu').removeClass('navbar-fixed');
+            }
+        })
+    }
+}
+
+navbarFixed();
+
